234d3ec4425c92c1faa0836a2bbf82c27f9bee6b
Daniil Fedin <nniiyu@yandex.ru>
2022-12-13 13:21:38 +0300

    Added static files css and favicon icon

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug_str>/', views.group_posts),
+    path('', views.index, name='index'),
+    path('group/<slug:slug_str>/', views.group_posts, name='group_posts'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,9 +5,17 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     template = 'posts/index.html'
-    return HttpResponse(render(request, template))
+    context = {
+        'title': 'Последние обновления на сайте',
+        'text': 'Это главная страница проекта Yatube',
+    }
+    return HttpResponse(render(request, template, context))
 
 
 def group_posts(request, slug_str):
     template = 'posts/group_list.html'
-    return HttpResponse(render(request, template))
+    context = {
+        'title': 'Лев Толстой – зеркало русской революции',
+        'text': 'Здесь будет информация о группах проекта Yatube',
+    }
+    return HttpResponse(render(request, template, context))
