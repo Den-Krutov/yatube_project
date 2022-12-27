0e6841cd919480e650079ade84c8ae4ac9226c4d
Daniil Fedin <nniiyu@yandex.ru>
2022-12-27 20:31:13 +0300

    Fixed bugs

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -89,8 +89,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nniiyu25@gmail.com'
-EMAIL_HOST_PASSWORD = 'Iorvapzxeq25'
+EMAIL_HOST_PASSWORD = 'kseetugznpbbftnf'
