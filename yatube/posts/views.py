551c21db662ac7df7a2493198bc0f44d5d59c5b5
Daniil Fedin <nniiyu@yandex.ru>
2022-12-28 13:44:39 +0300

    Added navigation from pages

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,5 @@
 """Provides response to requests app posts."""
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
@@ -9,9 +10,12 @@ COUNT_POSTS_PAGE: int = 10
 def index(request):
     """Display all posts."""
     template = 'posts/index.html'
-    posts = Post.objects.select_related('author', 'group')[:COUNT_POSTS_PAGE]
+    posts = Post.objects.select_related('author', 'group')
+    paginator = Paginator(posts, COUNT_POSTS_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, template, context)
 
@@ -20,9 +24,15 @@ def group_posts(request, slug):
     """Display all posts group."""
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.select_related('author')[:COUNT_POSTS_PAGE]
+    posts = group.posts.select_related('author')
+    paginator = Paginator(posts, COUNT_POSTS_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+    }
     context = {
         'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, template, context)
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -44,6 +44,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.year.year',
             ],
         },
     },
@@ -89,10 +90,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'nniiyu25@gmail.com'
-EMAIL_HOST_PASSWORD = 'kseetugznpbbftnf'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
