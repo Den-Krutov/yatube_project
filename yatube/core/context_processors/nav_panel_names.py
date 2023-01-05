552ee7d7eb121cb068a0bbdde0ff67aac8549c6f
Daniil Fedin <nniiyu@yandex.ru>
2023-01-05 12:45:47 +0300

    Fixing with PEP8

--- yatube/core/context_processors/nav_panel_names.py
+++ yatube/core/context_processors/nav_panel_names.py
@@ -1,16 +1,16 @@
 def nav_panel_names(request):
     list = [{'url': 'about:author', 'name': 'Об авторе'},
-            {'url': 'about:tech', 'name': 'Технологии'},]
+            {'url': 'about:tech', 'name': 'Технологии'}]
     auth_list = list.copy() + (
         [
-            {'url': 'posts:post_create', 'name':'Новая запись'},
+            {'url': 'posts:post_create', 'name': 'Новая запись'},
             {'url': 'users:password_change', 'name': 'Изменить пароль'},
             {'url': 'users:logout', 'name': 'Выйти'},
         ]
     )
     unauth_list = list.copy() + (
         [
-            {'url': 'users:login', 'name':'Войти'},
+            {'url': 'users:login', 'name': 'Войти'},
             {'url': 'users:signup', 'name': 'Регистрация'},
         ]
     )
