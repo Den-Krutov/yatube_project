b116373ae95b045fe7faf9510556d114c5e147c0
Daniil Fedin <nniiyu@yandex.ru>
2023-01-07 23:22:15 +0300

    Optimizing queries to db

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -26,7 +26,7 @@ def group_posts(request, slug):
     """Display all posts group."""
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.select_related('author', 'group')
+    posts = group.posts.select_related('author')
     paginator = Paginator(posts, COUNT_POSTS_PAGE)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -40,7 +40,7 @@ def group_posts(request, slug):
 def profile(request, username):
     template = 'posts/profile.html'
     author = get_object_or_404(User, username=username)
-    posts = author.posts.select_related('author', 'group')
+    posts = author.posts.select_related('group')
     paginator = Paginator(posts, COUNT_POSTS_PAGE)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
