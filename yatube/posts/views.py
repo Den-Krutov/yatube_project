38c571adb452013f2c84efb2e74be88231457036
Daniil Fedin <nniiyu@yandex.ru>
2023-01-09 13:19:03 +0300

    Added welkom email after signup

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,6 @@
 """Provides response to requests app posts."""
+from functools import partial
+
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
@@ -10,85 +12,69 @@ COUNT_POSTS_PAGE: int = 10
 
 
 def paginator(request, objects):
+    """Converts objects in instance Page with request"""
     paginator = Paginator(objects, COUNT_POSTS_PAGE)
     page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return page_obj
+    return paginator.get_page(page_number)
 
 
 def index(request):
     """Display all posts."""
-    template = 'posts/index.html'
-    posts = Post.objects.select_related('author', 'group')
-    context = {
-        'page_obj': paginator(request, posts),
-    }
-    return render(request, template, context)
+    posts = Post.objects.prefetch_related('author', 'group')
+    return render(request,
+                  'posts/index.html',
+                  context={'page_obj': paginator(request, posts)})
 
 
 def group_posts(request, slug):
     """Display all posts group."""
-    template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.select_related('author')
-    context = {
-        'group': group,
-        'page_obj': paginator(request, posts),
-    }
-    return render(request, template, context)
+    posts = group.posts.prefetch_related('author')
+    return render(request,
+                  'posts/group_list.html',
+                  context={'group': group,
+                           'page_obj': paginator(request, posts)})
 
 
 def profile(request, username):
-    template = 'posts/profile.html'
     author = get_object_or_404(User, username=username)
-    posts = author.posts.select_related('group')
-    context = {
-        'author': author,
-        'page_obj': paginator(request, posts),
-    }
-    return render(request, template, context)
+    posts = author.posts.prefetch_related('group')
+    return render(request,
+                  'posts/profile.html',
+                  context={'group': author,
+                           'page_obj': paginator(request, posts)})
 
 
 def post_detail(request, post_id):
-    template = 'posts/post_detail.html'
-    post = get_object_or_404(Post.objects.select_related('author', 'group'),
+    post = get_object_or_404(Post.objects.prefetch_related('author', 'group'),
                              pk=post_id)
-    context = {
-        'post': post,
-    }
-    return render(request, template, context)
+    return render(request, 'posts/post_detail.html', context={'post': post})
 
 
 @login_required
 def post_create(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('posts:profile', post.author.username)
-    else:
-        form = PostForm()
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', post.author.username)
     return render(request, 'posts/create_post.html', context={'form': form})
 
 
 @login_required
 def post_edit(request, post_id):
-    post = get_object_or_404(Post.objects.select_related('author', 'group'),
+    post = get_object_or_404(Post.objects.prefetch_related('author', 'group'),
                              pk=post_id)
-    redirected_page = redirect('posts:post_detail', post_id=post.pk)
-    if request.user == post.author:
-        if request.method == 'POST':
-            form = PostForm(request.POST, instance=post)
-            if form.is_valid():
-                form.save()
-                return redirected_page
-        else:
-            form = PostForm(instance=post)
-        return render(request,
-                      'posts/create_post.html',
-                      context={'post': post,
-                               'form': form,
-                               'is_edit': True})
-    return redirected_page
+    get_redirected_page = partial(redirect,
+                                  'posts:post_detail',
+                                  post_id=post.pk)
+    if request.user != post.author:
+        return get_redirected_page()
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return get_redirected_page()
+    return render(request,
+                  'posts/create_post.html',
+                  context={'post': post, 'form': form, 'is_edit': True})
--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('logout/',
          LogoutView.as_view(template_name='users/logged_out.html'),
          name='logout'),
-    path('signup/', views.SingUp.as_view(), name='signup'),
+    path('signup/', views.sing_up, name='signup'),
     path('password_change/',
          PasswordChangeView.as_view(template_name='users/'
                                                   'password_change_form.html'),
